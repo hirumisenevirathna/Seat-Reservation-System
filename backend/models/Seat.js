476e472063c64b3af290646e25dd6c91caeba224
HIRUMI SENEVIRATHNA <120644649+hirumisenevirathna@users.noreply.github.com>
2025-07-26 10:18:41 +0530

    admin

--- backend/models/Seat.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const SeatSchema = new mongoose.Schema({
-  seatNumber: String,
-  date: String, 
-  email: String, 
-});
-
-module.exports = mongoose.model('Seat', SeatSchema);
--- backend/models/SeatConfig.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const seatConfigSchema = new mongoose.Schema({
-  totalSeats: { type: Number, required: true, default: 40 },
-  disabledSeats: [{ type: String }], // List of seat numbers that are disabled
-});
-
-module.exports = mongoose.model("SeatConfig", seatConfigSchema);
